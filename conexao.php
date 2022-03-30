56fe5aa85b9ff3b94eaa0fc9f163556c2e5aa6b4
diogo-gb <80366250+diogo-gb@users.noreply.github.com>
2022-03-30 20:52:50 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php
+
+//host, usuário, senha, nome do banco
+
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'bab9256f606fd9', '8b3954cb', 'heroku_ef22c5b0f15dadc') or die('Erro ao conectar ao banco de dados');
